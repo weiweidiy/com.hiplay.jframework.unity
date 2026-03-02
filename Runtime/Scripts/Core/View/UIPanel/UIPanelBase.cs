ea66ab26d90e77afed554f967ce860c40732f671
jichunwei <49595272@qq.com>
2026-03-02 16:35:03 +0800

    update

--- Runtime/Scripts/Core/View/UIPanel/UIPanelBase.cs
+++ Runtime/Scripts/Core/View/UIPanel/UIPanelBase.cs
@@ -1,12 +1,68 @@
 ﻿
 using Cysharp.Threading.Tasks;
 using deVoid.UIFramework;
-using Game.Common;
 using System;
-using UnityEngine;
 
 namespace JFramework.Unity
 {
+    /// <summary>
+    /// 不带属性的UI面板基类，并且会发送面板显示事件
+    /// </summary>
+    public abstract class UIPanelBase : APanelController
+    {
+        public event Action<UIPanelBase> onPanelEnable;
+        public event Action<UIPanelBase> onPanelShow;
+        public event Action<UIPanelBase> onPanelHide;
+        public event Action<UIPanelBase> onPanelRefresh;
+
+        /// <summary>
+        /// 显示面板时调用，在Refresh之前调用
+        /// </summary>
+        protected virtual void OnPanelEnable() { onPanelEnable?.Invoke(this); }
+
+        /// <summary>
+        /// 刷新面板数据
+        /// </summary>
+        /// <param name="properties"></param>
+        protected virtual void OnPanelRefresh() { onPanelRefresh?.Invoke(this); }
+
+        /// <summary>
+        /// 面板显示完成，在Refresh之后调用
+        /// </summary>
+        protected virtual void OnPanelShow() { onPanelShow?.Invoke(this); }
+
+        /// <summary>
+        /// 面板被隐藏时调用
+        /// </summary>
+        protected virtual void OnPanelHide() { onPanelHide?.Invoke(this); }
+
+
+        /// <summary>
+        /// 面板属性被设置
+        /// </summary>
+        protected override void OnPropertiesSet()
+        {
+            base.OnPropertiesSet();
+
+            OnPanelEnable();
+
+            OnPanelRefresh();
+            // 等待一帧，确保UI位置更新完毕
+            //await UniTask.Yield();
+            OnPanelShow();
+        }
+
+        /// <summary>
+        /// 隐藏面板时调用
+        /// </summary>
+        protected override void WhileHiding()
+        {
+            base.WhileHiding();
+
+            OnPanelHide();
+        }
+
+    }
     /// <summary>
     /// UI面板基类，并且会发送面板显示事件
     /// </summary>
