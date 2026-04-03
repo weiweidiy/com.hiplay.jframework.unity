3db141a9935cfe1e7189ec29c395c072efbcaf0e
jichunwei <49595272@qq.com>
2026-04-03 15:14:24 +0800

    update

--- Runtime/Scripts/JFramework/Game/Language/ILanguageManager.cs
+++ Runtime/Scripts/JFramework/Game/Language/ILanguageManager.cs
@@ -6,7 +6,7 @@ namespace JFramework
     public interface ILanguageManager : ILanguage
     {
         event Action<ILanguage> onLanguageChanged;
-
+        void Initialize(ILanguage[] languages);
         ILanguage GetCurLanguage();
 
         ILanguage GetLanguage(string name);
